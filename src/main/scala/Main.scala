c80a91dee9f6774cfc9e48082c3168f4c48771a6
Julian Peeters <julianpeeters@gmail.com>
2013-09-23 13:18:31 -0700

    working example of an AvroType type to schema, and reading and writing binary io

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,24 @@
+//import com.gensler.scalavro.AvroType
 import com.gensler.scalavro.types.AvroType
+import com.gensler.scalavro.io.AvroTypeIO
+import scala.util.{Try, Success, Failure}
+import java.io._
+import models._
 
 object Main extends App{
-println(AvroType[Seq[String]].schema)
+
+  val myRecordType = AvroType[MyRecord]
+    println("schema: " + myRecordType.schema)
+
+  val myRecord = MyRecord("hello Scalavro")
+
+  val file = new File("data")
+  val outStream: OutputStream = new FileOutputStream(new File("data"))
+  myRecordType.io.write(myRecord, outStream)
+
+  val inStream: InputStream = new FileInputStream(file)
+  myRecordType.io.read(inStream) match {
+    case Success(readResult) => println("successfully deserialized: " + readResult)//readResult is instance of MyRecord
+    case Failure(cause)      => // handle failure...
+  }
 }
